3a987021213848b4972ac057070b1973b1951901
louiscarvalheiro-code <louiscarvalheiro@gmail.com>
2025-09-19 14:01:36 +0100

    Add files via upload

--- models.py
+++ models.py
@@ -1,41 +1,22 @@
-
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 class Material(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(200), nullable=False, unique=True)
-    preco = db.Column(db.Float, nullable=False, default=0.0)
-    transporte = db.Column(db.Float, nullable=False, default=0.0)
+    nome = db.Column(db.String(100), nullable=False)
+    preco = db.Column(db.Float, nullable=False)
 
 class Mistura(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(140), nullable=False, unique=True)
-    baridade = db.Column(db.Float, nullable=False, default=1.0)
-    componentes = db.relationship('MisturaMaterial', back_populates='mistura', cascade='all, delete-orphan')
+    nome = db.Column(db.String(100), nullable=False)
+    baridade = db.Column(db.Float, nullable=False)
+    composicoes = db.relationship("Composicao", backref="mistura", cascade="all, delete-orphan")
 
-class MisturaMaterial(db.Model):
+class Composicao(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    mistura_id = db.Column(db.Integer, db.ForeignKey('mistura.id'), nullable=False)
-    material_id = db.Column(db.Integer, db.ForeignKey('material.id'), nullable=False)
-    percentagem = db.Column(db.Float, nullable=False, default=0.0)
-    mistura = db.relationship('Mistura', back_populates='componentes')
-    material = db.relationship('Material')
+    mistura_id = db.Column(db.Integer, db.ForeignKey("mistura.id"), nullable=False)
+    material_id = db.Column(db.Integer, db.ForeignKey("material.id"), nullable=False)
+    percentagem = db.Column(db.Float, nullable=False, default=0)
 
-class Equipamento(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(140), nullable=False)
-    custo = db.Column(db.Float, nullable=False, default=0.0)
-    quantidade = db.Column(db.Integer, nullable=False, default=1)
-
-class Humano(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(140), nullable=False)
-    custo = db.Column(db.Float, nullable=False, default=0.0)
-    quantidade = db.Column(db.Integer, nullable=False, default=1)
-
-class Diverso(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(140), nullable=False, unique=True)
-    valor = db.Column(db.Float, nullable=False, default=0.0)
+    material = db.relationship("Material", backref="composicoes")
